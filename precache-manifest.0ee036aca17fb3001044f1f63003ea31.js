a1e8e8baca4faa663d3ee2f71dade11b79d739de
aba20 <askr1000@gmail.com>
2019-11-06 16:15:38 +0300

    Updates

---
+++ precache-manifest.0ee036aca17fb3001044f1f63003ea31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f5356a4b80263ab9df5ddc9e9c42580",
+    "url": "/travel-guide/index.html"
+  },
+  {
+    "revision": "367d5a52399c2551bb72",
+    "url": "/travel-guide/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "753aa663f5896393f083",
+    "url": "/travel-guide/static/css/main.b66afa79.chunk.css"
+  },
+  {
+    "revision": "367d5a52399c2551bb72",
+    "url": "/travel-guide/static/js/2.56881b2d.chunk.js"
+  },
+  {
+    "revision": "753aa663f5896393f083",
+    "url": "/travel-guide/static/js/main.d3e3dec5.chunk.js"
+  },
+  {
+    "revision": "06850274ba6a1e63cb7b",
+    "url": "/travel-guide/static/js/runtime-main.0529a29a.js"
+  }
+]);
